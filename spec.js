01df957f2f97dbafee00a3371dc6b8e70969caa9
Usman <m.usmanrafaqat@gmail.com>
2021-03-03 23:14:02 +0500

    3sec delay in exsiting functionallity

--- spec.js
+++ spec.js
@@ -6,7 +6,12 @@ it('hello how ar e', function(){
   browser.get("http://www.protractortest.org/#/",20000);
   element(By.id('drop4')).click();
   expect(browser.getTitle()).toEqual('Protractor - end-to-end testing for AngularJS');
-  
+  browser.driver.sleep(3000);
+  element(By.id('drop2')).click();
+  browser.driver.sleep(3000);
+  browser.driver.sleep(3000);
+  browser.driver.sleep(3000);
+
 
 
 });
